02e91b7bf79f2afea14f262b96ee7ac3837f4ea3
Emmanuel Wambua <mantelmanu31@gmail.com>
2026-02-23 13:31:41 +0300

    Fixed Pagination Buttons

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,29 +3,20 @@ import ReactPaginate from 'react-paginate';
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
 
-  return <ReactPaginate
-  containerclassName="pagination justify-content-center gap-2 my-4"
-
-  pageclassName="page-item"
-  pageLinkclassName="page-link"
-
-  previousclassName="page-item"
-  previousLinkclassName="page-link"
-
-  nextclassName="page-item"
-  nextLinkclassName="page-link"
-
-  activeclassName="active"
-
-  previousLabel="Prev"
-  nextLabel="Next"
-
-  forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
-  onPageChange={(data) => {
-    setPageNumber(data.selected + 1);
-  }}
-  pageCount={info?.pages}
-/>
+  return<ReactPaginate 
+  className='pagination justify-content-center gap-4 my-4' 
+  nextLabel="Next" 
+  previousLabel="Prev" 
+  nextClassName="btn btn-primary" 
+  previousClassName="btn btn-primary" 
+  previousLinkClassName="text-white link-opacity-0 text-decoration-none" 
+  nextLinkClassName="text-white link-opacity-0 text-decoration-none" 
+  pageClassName="page-item" 
+  pageLinkClassName="page-link" 
+  activeClassName="active" 
+  forcePage={pageNumber === 1 ? 0 : pageNumber - 1} 
+  onPageChange={(data) => { setPageNumber(data.selected + 1); }}
+   pageCount={info?.pages} />
 }
 
 export default Pagination
